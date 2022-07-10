79dfbf736043434f807f039adf0836d01879d4c4
Martin Varela <martin19957777@gmail.com>
2022-07-10 13:52:11 +0200

    social media buttons overlay on image

--- src/app/shared/components/image-viewer/image-viewer.component.ts
+++ src/app/shared/components/image-viewer/image-viewer.component.ts
@@ -10,13 +10,29 @@ import { Image } from '../../models/api-responses/image-list';
 })
 export class ImageViewerComponent {
 
+  public hasMediaLinks!: boolean;
+  public instagramUsername?: string;
+  public twitterUsername?: string;
+
   constructor(
     public dialogRef: MatDialogRef<ImageViewerComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Image
-  ) { }
+  ) {
+    this.hasMediaLinks = !!data.user.social.instagram_username || !!data.user.social.twitter_username;
+    this.instagramUsername = data.user.social.instagram_username;
+    this.twitterUsername = data.user.social.instagram_username;
+  }
 
   public close(): void {
     this.dialogRef.close();
+  }
+
+  public goToTwitterProfile(): void {
+    window.open(`https://www.twitter.com/${this.twitterUsername}`, "_blank")
+  }
+
+  public goToInstagramProfile(): void {
+    window.open(`https://www.instagram.com/${this.instagramUsername}`, "_blank")
   }
 
 }
