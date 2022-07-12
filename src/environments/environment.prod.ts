b13bf87d102ba3a057e2c8b169e5fea8e4e8e683
Martin Varela <martin19957777@gmail.com>
2022-07-12 19:49:12 +0200

    added production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  UNSPLASH_API_KEY: 'UH7lXl5xPBMexWzMkB82MXVdAlTK7qMpg5txHQp10VA',
+  UNSPLASH_BASE_URL: 'https://api.unsplash.com'
 };
