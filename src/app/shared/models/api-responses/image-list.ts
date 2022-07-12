6252491e56ef12dd86116a6c0ddfd1a1a50880f2
Martin Varela <martin19957777@gmail.com>
2022-07-12 15:27:41 +0200

    refactored image list interface

--- src/app/shared/models/api-responses/image-list.ts
+++ src/app/shared/models/api-responses/image-list.ts
@@ -24,238 +24,13 @@ export interface Image {
   sponsorship?: any;
   topic_submissions: Topicsubmissions;
   user: User;
-  tags: (Tag | Tags2 | Tags3 | Tags4 | Tags5)[];
-}
-
-interface Tags5 {
-  type: string;
-  title: string;
-  source?: Source3;
-}
-
-interface Tags4 {
-  type: string;
-  title: string;
-  source?: Source4;
-}
-
-interface Source4 {
-  ancestry: Ancestry3;
-  title: string;
-  subtitle: string;
-  description: string;
-  meta_title: string;
-  meta_description: string;
-  cover_photo: Coverphoto4;
-}
-
-interface Coverphoto4 {
-  id: string;
-  created_at: string;
-  updated_at: string;
-  promoted_at: string;
-  width: number;
-  height: number;
-  color: string;
-  blur_hash: string;
-  description: string;
-  alt_description: string;
-  urls: Urls;
-  links: Links;
-  categories: any[];
-  likes: number;
-  liked_by_user: boolean;
-  current_user_collections: any[];
-  sponsorship?: any;
-  topic_submissions: Topicsubmissions5;
-  user: User5;
-}
-
-interface User5 {
-  id: string;
-  updated_at: string;
-  username: string;
-  name: string;
-  first_name: string;
-  last_name: string;
-  twitter_username: string;
-  portfolio_url: string;
-  bio: string;
-  location?: string;
-  links: Links2;
-  profile_image: Profileimage;
-  instagram_username: string;
-  total_collections: number;
-  total_likes: number;
-  total_photos: number;
-  accepted_tos: boolean;
-  for_hire: boolean;
-  social: Social4;
-}
-
-interface Topicsubmissions5 {
-  nature?: Texturespatterns;
-}
-
-interface Ancestry3 {
-  type: Type;
-  category: Type;
-  subcategory?: Type;
-}
-
-interface Tags3 {
-  type: string;
-  title: string;
-  source: Source3;
-}
-
-interface Source3 {
-  ancestry: Ancestry;
-  title: string;
-  subtitle: string;
-  description: string;
-  meta_title: string;
-  meta_description: string;
-  cover_photo: Coverphoto3;
-}
-
-interface Coverphoto3 {
-  id: string;
-  created_at: string;
-  updated_at: string;
-  promoted_at: string;
-  width: number;
-  height: number;
-  color: string;
-  blur_hash: string;
-  description: string;
-  alt_description: string;
-  urls: Urls;
-  links: Links;
-  categories: any[];
-  likes: number;
-  liked_by_user: boolean;
-  current_user_collections: any[];
-  sponsorship?: any;
-  topic_submissions: Topicsubmissions4;
-  user: User4;
-}
-
-interface User4 {
-  id: string;
-  updated_at: string;
-  username: string;
-  name: string;
-  first_name: string;
-  last_name: string;
-  twitter_username: string;
-  portfolio_url: string;
-  bio: string;
-  location?: any;
-  links: Links2;
-  profile_image: Profileimage;
-  instagram_username: string;
-  total_collections: number;
-  total_likes: number;
-  total_photos: number;
-  accepted_tos: boolean;
-  for_hire: boolean;
-  social: Social4;
-}
-
-interface Social4 {
-  instagram_username: string;
-  portfolio_url: string;
-  twitter_username: string;
-  paypal_email?: any;
-}
-
-interface Topicsubmissions4 {
-}
-
-interface Tags2 {
-  type: string;
-  title: string;
-  source: Source2;
-}
-
-interface Source2 {
-  ancestry: Ancestry2;
-  title: string;
-  subtitle: string;
-  description: string;
-  meta_title: string;
-  meta_description: string;
-  cover_photo: Coverphoto2;
-}
-
-interface Coverphoto2 {
-  id: string;
-  created_at: string;
-  updated_at: string;
-  promoted_at: string;
-  width: number;
-  height: number;
-  color: string;
-  blur_hash: string;
-  description: string;
-  alt_description: string;
-  urls: Urls;
-  links: Links;
-  categories: any[];
-  likes: number;
-  liked_by_user: boolean;
-  current_user_collections: any[];
-  sponsorship?: any;
-  topic_submissions: Topicsubmissions3;
-  user: User3;
-}
-
-interface User3 {
-  id: string;
-  updated_at: string;
-  username: string;
-  name: string;
-  first_name: string;
-  last_name: string;
-  twitter_username?: string;
-  portfolio_url: string;
-  bio: string;
-  location?: string;
-  links: Links2;
-  profile_image: Profileimage;
-  instagram_username: string;
-  total_collections: number;
-  total_likes: number;
-  total_photos: number;
-  accepted_tos: boolean;
-  for_hire: boolean;
-  social: Social3;
-}
-
-interface Social3 {
-  instagram_username: string;
-  portfolio_url: string;
-  twitter_username?: string;
-  paypal_email?: any;
-}
-
-interface Topicsubmissions3 {
-  'color-of-water'?: Texturespatterns;
-  architecture?: Texturespatterns;
-  wallpapers?: Texturespatterns;
-}
-
-interface Ancestry2 {
-  type: Type;
-  category?: Type;
-  subcategory?: Type;
+  tags: Tag[];
 }
 
 interface Tag {
   type: string;
-  title: string;
-  source: Source;
+  title?: string;
+  source?: Source;
 }
 
 interface Source {
@@ -272,7 +47,7 @@ interface Coverphoto {
   id: string;
   created_at: string;
   updated_at: string;
-  promoted_at: string;
+  promoted_at?: string;
   width: number;
   height: number;
   color: string;
@@ -286,47 +61,14 @@ interface Coverphoto {
   liked_by_user: boolean;
   current_user_collections: any[];
   sponsorship?: any;
-  topic_submissions: Topicsubmissions2;
-  user: User2;
-}
-
-interface User2 {
-  id: string;
-  updated_at: string;
-  username: string;
-  name: string;
-  first_name: string;
-  last_name: string;
-  twitter_username?: any;
-  portfolio_url: string;
-  bio: string;
-  location: string;
-  links: Links2;
-  profile_image: Profileimage;
-  instagram_username: string;
-  total_collections: number;
-  total_likes: number;
-  total_photos: number;
-  accepted_tos: boolean;
-  for_hire: boolean;
-  social: Social2;
-}
-
-interface Social2 {
-  instagram_username: string;
-  portfolio_url: string;
-  twitter_username?: any;
-  paypal_email?: any;
-}
-
-interface Topicsubmissions2 {
-  'textures-patterns': Texturespatterns;
+  topic_submissions: Topicsubmissions;
+  user: User;
 }
 
 interface Ancestry {
-  type: Type;
-  category: Type;
-  subcategory: Type;
+  type?: Type;
+  category?: Type;
+  subcategory?: Type;
 }
 
 interface Type {
@@ -345,7 +87,7 @@ interface User {
   portfolio_url?: string;
   bio?: string;
   location?: string;
-  links: Links2;
+  links: Links;
   profile_image: Profileimage;
   instagram_username?: string;
   total_collections: number;
@@ -369,14 +111,14 @@ interface Profileimage {
   large: string;
 }
 
-interface Links2 {
-  self: string;
-  html: string;
-  photos: string;
-  likes: string;
-  portfolio: string;
-  following: string;
-  followers: string;
+interface Links {
+  self?: string;
+  html?: string;
+  photos?: string;
+  likes?: string;
+  portfolio?: string;
+  following?: string;
+  followers?: string;
 }
 
 interface Topicsubmissions {
@@ -394,13 +136,6 @@ interface Texturespatterns {
 interface Wallpapers {
   status: string;
   approved_on?: string;
-}
-
-interface Links {
-  self: string;
-  html: string;
-  download: string;
-  download_location: string;
 }
 
 interface Urls {
